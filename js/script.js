bba517728d3264b17ebd05295a2cc82517cb137a
gabriel aleixo <aleixo.gabriel.aleixo@gmail.com>
2024-06-14 15:07:46 -0300

    responsividade

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 window.addEventListener("scroll", function () {
     let header = document.querySelector('#header')
     header.classList.toggle('scrollh', window.scrollY > 1)
-})
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const navMenu = document.getElementById('nav-menu');
+
+    hamburger.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+    });
+});
